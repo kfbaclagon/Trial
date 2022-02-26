592c5f39a571bfaca35d0ca22cf74ca5fdcdf586
AjEvang <201911520@feualabang.edu.ph>
2022-02-26 23:11:23 +0800

    added quote

--- main.cpp
+++ main.cpp
@@ -5,12 +5,14 @@ using namespace std;
 void tine();
 void kingsman();
 void aila();
+void quote();
 
 int main(){
     cout<< "Hello World"<<endl;
     tine();
     kingsman();
     aila();
+    quote();
  
     return 0;
 }
@@ -35,5 +37,9 @@ void aila(){
 	
 }
 
+void quote(){
+	
+	cout << "Trust the timing of your life" << endl;
+}
 
 
