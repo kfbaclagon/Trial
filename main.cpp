f21bc28a1b0ef51090959372b914d8da4e966e14
kfbaclagon <201910322@feualabang.edu.ph>
2022-02-26 20:47:21 +0800

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+void tine();
+
+int main(){
+    cout<< "Hello World"<<endl;
+    tine();
+ 
+    return 0;
+}
+
+
+void tine(){
+	cout<< "Tine" << endl;
+	
+}
+
+
+
+
